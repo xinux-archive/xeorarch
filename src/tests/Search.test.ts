3d90191700539ba7ab3b993c2a405377625a2017
UwUssimo Robinson <uwussimo@icloud.com>
2022-01-22 09:37:33 +0500

    extended tests

--- src/tests/Search.test.ts
+++ src/tests/Search.test.ts
@@ -1,8 +1,6 @@
 import * as Search from '../search'
 
 test('Query Search', async () => {
-    jest.setTimeout(50000)
-
     const request = await Search.search('linux')
 
     // Check for packages to have required keys
