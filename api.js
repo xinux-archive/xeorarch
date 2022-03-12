4ed15b245527962ed729c57d2746a0f65d09fd1f
UwUssimo Robinson <uwussimo@icloud.com>
2022-03-12 15:39:30 +0500

    removing trash

--- api.js
+++
@@ -1,4 +0,0 @@
-(async () => {
-    const xeorarch = require("./lib/mod.js");
-    console.log(await xeorarch.AUR.search("hello"));
-})();
--- api.ts
+++
@@ -1,3 +0,0 @@
-import { AUR } from "./src/mod.ts";
-
-console.log(await AUR.search("hello"));
