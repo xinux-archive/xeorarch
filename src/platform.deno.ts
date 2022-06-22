63108b8b148ae387ba9d12e52fceb8148d0d4400
UwUssimo Robinson <uwussimo@icloud.com>
2022-06-22 16:01:31 +0500

    Update platform.deno.ts

--- src/platform.deno.ts
+++ src/platform.deno.ts
@@ -1,2 +1,2 @@
 export { default as Fuse } from "https://deno.land/x/fuse@v6.4.1/dist/fuse.esm.js";
-export { cheerio } from "https://deno.land/x/cheerio@1.0.4/mod.ts";
+export { cheerio } from "https://raw.githubusercontent.com/Bestulo/deno-cheerio/main/mod.ts";
